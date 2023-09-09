1a9c60fe4abb44c74b89b5d63a363f4204ea1c25
Mr. Hasan <hasanshahriar32@gmail.com>
2023-09-10 01:37:36 +0600

    update

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -381,12 +381,12 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://shahriarhasan.vercel.app"
+          href="https://paradox-bd.com"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          Shahriar Hasan
+          Paradox BD
         </a>
         .{' '}
         {t(
